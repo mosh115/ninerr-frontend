75f8d02d6c5f28d20ecc6616623e241227cb6e8e
Moshe <moshrozner@gmail.com>
2022-03-10 09:18:29 +0200

    test 1

--- src/cmps/popular-service-card.jsx
+++ src/cmps/popular-service-card.jsx
@@ -7,14 +7,11 @@ export function PopularServiceCard(props) {
     const { img, title, subTitle } = popularService
 
     return (
-        <article >
-            <li className='popular-service-card'>
-                <img src={img} alt={title} />
-                <h5>{subTitle}</h5>
-                <h3>{title}</h3>
-                <div className='shadow'></div>
-            </li>
-
+        <article className='popular-service-card'>
+            <img src={img} alt={title} />
+            <h5>{subTitle}</h5>
+            <h3>{title}</h3>
+            <div className='shadow'></div>
         </article>
     )
 }
--- src/cmps/popular-service-list.jsx
+++ src/cmps/popular-service-list.jsx
@@ -6,10 +6,33 @@ import VoiceOver from '../assets/img/home-page/popular-professional-services/voi
 import VideoExplainer from '../assets/img/home-page/popular-professional-services/video-explainer.jpg';
 import LogoDesign from '../assets/img/home-page/popular-professional-services/logo-design.jpg';
 import SocialMedia from '../assets/img/home-page/popular-professional-services/social-media.jpg';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import { PopularServiceCard } from './popular-service-card';
 
 export function PopularServiceList() {
+    const responsive = {
+        wide: {
+            breakpoint: { max: 3000, min: 1560 },
+            items: 5,
+        },
+        desktop: {
+            breakpoint: { max: 1560, min: 900 },
+            items: 4,
+            slidesToSlide: 1
+        },
+        tablet: {
+            breakpoint: { max: 900, min: 600 },
+            items: 3,
+            slidesToSlide: 3
+        },
+        mobile: {
+            breakpoint: { max: 600, min: 0 },
+            items: 1,
+            slidesToSlide: 1
+        }
+    };
 
     const popularServices = [
         { img: Wordpress, title: 'Wordpress', subTitle: 'Customize your site' },
@@ -20,8 +43,28 @@ export function PopularServiceList() {
     ]
 
     return (
-        <section>
-            <ul className='popular-service-list clean-list'>
+        <section className='popular-service-list'>
+            <Carousel
+                responsive={responsive}
+                draggable={false}
+                infinite={true}
+                keyBoardControl={false}
+                focusOnSelect={true}
+                shouldResetAutoplay={false}
+
+                removeArrowOnDeviceType={["wide"]}
+            >
+                {popularServices.map(service => {
+                    return (
+                        <Link key={service.title} to={`/explore?filter=tags:${service.title}`}>
+                            <PopularServiceCard popularService={service} />
+                        </Link>
+                    )
+                }
+                )}
+            </Carousel>
+
+            {/* <ul className='popular-service-list clean-list'>
                 {popularServices.map((service, idx) => {
                     return (
                         <Link key={idx} to={`/explore?filter=tags:${service.title}`}>
@@ -30,7 +73,7 @@ export function PopularServiceList() {
                     )
                 }
                 )}
-            </ul>
+            </ul> */}
         </section>
     )
 }
